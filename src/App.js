491dab21dbd7dae882e45dae89c8aaabdd193fe8
EzAmmar <ezammar@Aspire-E5-575G-8c642e2b.localdomain>
2023-01-14 23:28:35 +0200

    add Protected Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import ProtectedRoute from "./components/ProtectedRoute";
 import AllUser from "./pages/AllUser";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
@@ -32,76 +33,83 @@ export default class App extends Component {
     return (
       <>
         <Routes>
-          <Route index element={<Navigate to="/login" />} />
+          {/* <Route index element={<Navigate to="/login" />} /> */}
           <Route
             path="/login"
             element={
               <>
                 {this.state.isAuthorized ? (
-                  <Navigate to="/home" />
+                  <Navigate to="/" />
                 ) : (
                   <Login login={this.login} />
                 )}
               </>
             }
           />
-          <Route
-            path="/home"
-            element={
-              this.state.isAuthorized ? (
-                <Home logout={this.logout} />
-              ) : (
-                <Login login={this.login} />
-              )
-            }
-          />
+
           <Route
             path="/signUp"
             element={
               <>
                 {this.state.isAuthorized ? (
-                  <Navigate to="/home" />
+                  <Navigate to="/" />
                 ) : (
                   <SignUp login={this.login} />
                 )}
               </>
             }
           />
-          <Route path="/profile" element={<Profile />} />
 
           <Route
-            path="/all-user"
+            path="/"
             element={
-              <>
-                {this.state.isAuthorized ? (
-                  localStorage.getItem("isAdmin") === "true" ? (
-                    <AllUser />
-                  ) : (
-                    <h1>You do not have permission to access this page</h1>
-                  )
-                ) : (
-                  <Login login={this.login} />
-                )}
-              </>
+              <ProtectedRoute
+                isAuthorized={this.state.isAuthorized}
+                logout={this.logout}
+              />
             }
-          />
+          >
+            <Route index element={<Home logout={this.logout} />} />
+            <Route path="profile" element={<Profile />} />
+          </Route>
 
-          <Route
-            path="/single-user/:id"
-            element={
-              <>
-                {this.state.isAuthorized ? (
-                  localStorage.getItem("isAdmin") === "true" ? (
-                    <SingleUser />
-                  ) : (
-                    <h1>You do not have permission to access this page</h1>
-                  )
-                ) : (
-                  <Login login={this.login} />
-                )}
-              </>
-            }
-          />
+          {localStorage.getItem("isAdmin") === "true" && (
+            <>
+              <Route
+                path="/all-user"
+                element={
+                  <>
+                    {this.state.isAuthorized ? (
+                      localStorage.getItem("isAdmin") === "true" ? (
+                        <AllUser />
+                      ) : (
+                        <h1>You do not have permission to access this page</h1>
+                      )
+                    ) : (
+                      <Login login={this.login} />
+                    )}
+                  </>
+                }
+              />
+
+              <Route
+                path="/single-user/:id"
+                element={
+                  <>
+                    {this.state.isAuthorized ? (
+                      localStorage.getItem("isAdmin") === "true" ? (
+                        <SingleUser />
+                      ) : (
+                        <h1>You do not have permission to access this page</h1>
+                      )
+                    ) : (
+                      <Login login={this.login} />
+                    )}
+                  </>
+                }
+              />
+            </>
+          )}
 
           <Route path="*" element={<h1>page not found</h1>} />
         </Routes>
